ccb33159b8f2cbb5352b3cbd3f38e288174872ca
Kim Waters <kimwaters1996@gmail.com>
2020-12-14 17:53:12 +0000

    Change to secondname

--- TeamPowered/Person.cs
+++ TeamPowered/Person.cs
@@ -7,7 +7,7 @@ namespace TeamPowered
     class Person
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string SecondName { get; set; }
         public string Address { get; set; }
     }
 }
